037777cf854857811df010d267a731aeeb411307
hacrone <oheppy@gmail.com>
2017-03-02 00:02:03 +0800

    add twitter helper

---
+++ src/View/Helper/TwitterHelper.php
@@ -0,0 +1,24 @@
+<?php
+namespace CakeSocial\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Twitter helper
+ */
+class TwitterHelper extends Helper
+{
+
+	/**
+	 * Default configuration.
+	 *
+	 * @var array
+	 */
+	protected $_defaultConfig = [
+		'app_id' => '',
+		'app_secret' => '',
+		'callback' => '',
+	];
+
+}
