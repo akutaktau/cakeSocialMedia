b9bf2f5d360143fcc0702f3b36bfdd9f16e74569
Jasdy Syarman Mohd Saari <akutaktau@gmail.com>
2017-03-01 23:27:10 +0800

    Update routes.php
    Change to cake-social-media

--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,8 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
 Router::plugin(
-    'CakeSocial',
-    ['path' => '/cake-social'],
+    'CakeSocialMedia',
+    ['path' => '/cake-social-media'],
     function (RouteBuilder $routes) {
         $routes->fallbacks('DashedRoute');
     }
